560feb05decb16123bb246f6c235aabd1dc41094
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-15 10:32:03 +0100

    fix struttura

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,7 @@
 // Milestone 5 - opzionale
 // Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
 
-// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatt
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 
 const { createApp } = Vue
 
